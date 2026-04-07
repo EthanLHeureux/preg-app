aa5d4e97aac65929b7dae588f5a0bcc5c4d10858
EthanLHeureux <142943704+EthanLHeureux@users.noreply.github.com>
2026-04-07 14:30:29 -0500

    Create: Front End

---
+++ Preg-App Frontend/Preg-App Frontend/Preg_App_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Preg_App_FrontendApp.swift
+//  Preg-App Frontend
+//
+//  Created by Ethan L'Heureux on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Preg_App_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
